457f049a9ad2804a93696c50e710a17b3e668cd1
TracyBaraza <barazatracy16@gmal.com>
2019-04-16 15:00:34 +0300

    student details

---
+++ student.py
@@ -0,0 +1,26 @@
+class Student:
+	def __init__(self,first_name,second_name,age):
+		self.first_name = first_name
+		self.second_name = second_name
+		self.age = age
+
+
+	def full_name(self):
+		name = self.first_name + self.second_name
+		return name
+
+
+	def year_of_birth(self):
+		return 2019-self.age
+
+
+	def greeting(self):
+		return "hello,{} you were born in {}".format(self.first_name,2019-self.age)
+
+
+	def initials(self):
+		ints= self.first_name[0] + self.second_name[0]
+		return ints
+
+		
+  
